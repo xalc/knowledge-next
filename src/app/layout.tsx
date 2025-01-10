a579c2a2baa43411cce365e7a57bdbca9a226260
xalc <chao1990li@gmail.com>
2025-01-10 20:40:19 +0800

    add google verify message

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export const metadata: Metadata = {
   icons: "/images/HX.svg",
 };
 
+
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>

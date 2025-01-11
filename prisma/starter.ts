4d2a9578374a23d6e2c624cc97ca7cdedda675c1
xalc <chao1990li@gmail.com>
2025-01-11 16:45:55 +0800

    build issue

--- prisma/starter.ts
+++ prisma/starter.ts
@@ -15,12 +15,3 @@ async function main() {
   console.dir(allPosts, { depth: null });
 }
 
-main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async e => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });

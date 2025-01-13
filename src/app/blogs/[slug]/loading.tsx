b66f187106fae86101b54182a8c7c8745b52f525
xalc <chao1990li@gmail.com>
2025-01-13 18:00:05 +0800

    lint stage

--- src/app/blogs/[slug]/loading.tsx
+++ src/app/blogs/[slug]/loading.tsx
@@ -3,11 +3,10 @@ import { Skeleton } from "@/components/ui/skeleton";
 export default function Loading() {
   return (
     <div className="container mx-auto">
-      <div className="mx-12 my-16 ">
+      <div className="mx-12 my-16">
         <Skeleton className="h-8 w-[100px]" />
       </div>
       <div className="mx-12 grid grid-cols-1 gap-10 xl:grid-cols-[1fr_240px]">
-
         <article className="mb-36 space-y-8">
           <Skeleton className="h-12 w-full" />
           <Skeleton className="h-12 w-full" />
@@ -18,7 +17,6 @@ export default function Loading() {
 
         <div className="hidden 2xl:block">
           <div className="sticky top-20 space-y-6">
-
             <Skeleton className="h-16 w-full" />
             <Skeleton className="h-8 w-full" />
             <Skeleton className="h-8 w-full" />
@@ -28,6 +26,5 @@ export default function Loading() {
         </div>
       </div>
     </div>
-
   );
 }

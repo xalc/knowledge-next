6b7315be4af83696a5c4464a08863fa2b8cbff40
xalc <chao1990li@gmail.com>
2025-01-30 15:45:56 +0800

    update sitemap

--- src/app/docs/sitemap.ts
+++ src/app/docs/sitemap.ts
@@ -2,10 +2,10 @@ import { getAllSlugs } from "@/lib/docs";
 import { MetadataRoute } from "next";
 const BASE_URL = process.env.PRODUCT_DOMAIN;
 
-// export async function generateSitemaps() {
-//   // Fetch the total number of products and calculate the number of sitemaps needed
-//   return [{ id: 1 }];
-// }
+export async function generateSitemaps() {
+  // Fetch the total number of products and calculate the number of sitemaps needed
+  return [{ id: 1 }];
+}
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const slugs = await getAllSlugs();

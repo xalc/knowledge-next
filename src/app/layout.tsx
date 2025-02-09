9641368aa8e1ff9fea58c9e0506a917164e0e8f8
xalc <chao1990li@gmail.com>
2025-02-09 18:36:27 +0800

    update manifest

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,8 @@ export default async function RootLayout({ children }: { children: React.ReactNo
   return (
     <html lang="en" suppressHydrationWarning>
       <link rel="manifest" href="/manifest.json" />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-title" content="blog"></meta>
       <link rel="apple-touch-icon" href="/favor/apple-touch-icon.png" />
       <body>
         <ThemeProvider

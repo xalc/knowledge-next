989c5c50b7d2a32f430f2a4d2325840d90341870
xalc <chao1990li@gmail.com>
2025-02-09 11:51:29 +0800

    pwa

--- src/components/pwa/pwa.tsx
+++ src/components/pwa/pwa.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 import { useState, useEffect } from "react";
-import { subscribeUser, unsubscribeUser, sendNotification } from "./actions";
+import { subscribeUser, unsubscribeUser, sendNotification } from "@/actions/pwa";
 function urlBase64ToUint8Array(base64String: string) {
   const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
   const base64 = (base64String + padding).replace(/-/g, "+").replace(/_/g, "/");
